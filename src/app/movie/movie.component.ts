7d263fb4c3fc0a9724dd9675c79b53dca072e6d9
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2024-05-21 20:10:03 -0300

    feat: cria estilos dos filmes

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../services/movie.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css'],
   standalone: true,
+  imports: [CommonModule],
 })
 export class MovieComponent implements OnInit {
   movies: any = [];
+  squares: any[] = [];
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.loadMovies();
+    this.calculateSquares();
+  }
+
+  calculateSquares() {
+    const movieListWidth = 1000;
+    const squareSize = 52;
+    const numberOfSquares = Math.floor(movieListWidth / squareSize);
+    this.squares = Array(numberOfSquares).fill(0);
   }
 
   loadMovies() {
